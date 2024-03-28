db88d05e35f513f6332a6a56c85f4097fd2f74b2
Eric Iwan <eric.iwan@proton.me>
2024-03-28 05:54:13 +0000

    outline a get request

--- event/src/app/api/events/getEventsByCategory/route.ts
+++ event/src/app/api/events/getEventsByCategory/route.ts
@@ -1,20 +1,62 @@
-import mongoose from 'mongoose';
+import dbConnect from "@/lib/mongo";
+import Event from "@/models/Event";
+import User from "@/models/User";
 import { NextRequest, NextResponse } from "next/server";
-import Events from '@/models/Event'
-import dbConnect from '@/lib/mongo/index'
+import * as jose from "jose";
 
-export async function GET(req: Request) {
-    console.log("geteventsbycategory GET request")
-    try {
-        await dbConnect();
-        const dbTest = await Events.find({})
-        console.log("thedb test",dbTest);
-        return NextResponse.json({ success: true });
-    } catch (err) {
-        console.log("Eventtest err", err)
-        return NextResponse.json({ success: false })
+// workflow:
+// check auth and get user id from mongo
+// get event from mongo and sort into lists by category
+// send data to client
+
+export async function GET(request: NextRequest) {
+  const skey: string = process.env.SECRETKEY!;
+  const cookie = request.cookies.get("accessToken");
+  const key = new TextEncoder().encode(skey);
+
+  if (!cookie) {
+    return NextResponse.json({
+      status: 400,
+      body: { error: "Please login, to redirect to login page" },
+    });
+  }
+
+  try {
+    await dbConnect();
+
+    const { payload } = await jose.jwtVerify(cookie.value, key, {});
+
+    console.log("payload", payload);
+
+    if (!payload.userid) {
+      return NextResponse.json({ error: "Invalid user" });
     }
+
+    const user = await User.findById(payload.userid);
+
+    console.log("user", user);
+
+    if (!user) {
+      return NextResponse.json({ error: "No such user in db" });
+    }
+
+    //const { eventId } = await request.json();
+
+
+    const event = await Event.find({});
+
+    console.log("event", event);
+
+    if (!event) {
+      return NextResponse.json({ error: "No events in db" });
+    }
+    
+    return NextResponse.json({ message: "Successfully joined" });
+  } catch (error) {
+    const err = error as Error;
+    console.log("error caught:", error);
+    console.log(err.name, err.message);
+    return NextResponse.json(error);
+  }
 }
-
-
 
